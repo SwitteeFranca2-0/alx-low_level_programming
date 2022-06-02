f4bdfe1e1e414665008da49cdcd674c6ca4dbb08
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-02 20:50:13 +0100

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+ * main - printing an array of characters.
+ *
+ * Return: Always a value 0.
+ */
+
+int main(void)
 {
 	char print_line[] = "with proper grammar, but the outcome is a piece of art,";
-	printf("%s\n",print_line);
-	return(0);
+
+	printf("%s\n", print_line);
+	return (0);
 }
