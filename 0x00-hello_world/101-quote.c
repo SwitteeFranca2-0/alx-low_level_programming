db392c814af5ed23c786d82ef6bcd249ac09318e
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-02 22:42:56 +0100

    C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+
+/**
+ * main - output a string without using the printf or put.
+ *
+ * Return: Always one.
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65);
+	return (1);
+}
+
