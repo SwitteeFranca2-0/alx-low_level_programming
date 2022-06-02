a1071ea54fad8ff2d710a4b8ad8830095749e1f8
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-02 19:54:58 +0100

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char c[] = "\"Programming is like building a multilingual puzzle.\n";
+	char c[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(c);
 return (0);
