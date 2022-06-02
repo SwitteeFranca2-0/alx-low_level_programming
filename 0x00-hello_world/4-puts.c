9625ed768843fe0db7dccb7a1e6461a2ed3e86cf
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-02 19:52:56 +0100

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints a string or array of characters.
+ *
+ * Return:  returning output zero.
+ */
 int main(void)
 {
-	char c[] = "Programming is like building a multilingual puzzle.\n";
+	char c[] = "\"Programming is like building a multilingual puzzle.\n";
+
 	puts(c);
 return (0);
 }
