38319e6cd6f4d7f3d37a5a735c39e9c76fa75274
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-03 22:21:02 +0100

    C

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@ int main(void)
 	int m;
 	int n;
 
-	for (m = 0; m < 100; m++)
+	for (m = 0; m < 99; m++)
 	{
-		for (n = m ; n < 100; n++)
+		for (n = m + 1 ; n < 100; n++)
 		{	putchar((m / 10) + '0');
 			putchar((m % 10) + '0');
 			putchar(' ');
 			putchar((n / 10) + '0');
 			putchar((n % 10) + '0');
-			if ((m == 99) && (n == 99))
+			if ((m == 98) && (n == 99))
 			{
 				continue;
 			}
