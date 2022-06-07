926fca5f94fbee2b40198546471627f4ea3ca205
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-08 00:26:56 +0100

    C

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - print times table.
+ * @n: variable
+ * Return: no return value
+ */
+
+void print_times_table(int n)
+{
+	int m;
+	int o;
+	int p;
+
+	if ((n > 0) && (n < 15))
+	{
+		for (m = 0; m <= n ; m++)
+		{
+			for (o = 0; o <= n; o++)
+			{
+				p = m * n;
+				if ((p > 9) && (p <= 99))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p / 10 + '0');
+					_putchar(p % 10 + '0');
+				}
+				else if (p > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p / 100) + '0');
+					_putchar((p / 10) % 10 + '0');
+					_putchar(p % 10 + '0');
+				}
+				else
+				{
+					_putchar(p + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+		}
+	}
+}
