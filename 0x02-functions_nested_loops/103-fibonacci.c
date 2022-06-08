67dd97e20d4e93f999cad4e0974e9a3f6da085de
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-08 01:36:01 +0100

    C

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonnaci.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long f1;
+	long f2;
+	long sum;
+	long sums;
+	int n;
+
+	f1 = 1;
+	f2 = 2;
+	sums = f1 + f2;
+	for (n = 2; n < 50; n++)
+	{
+		sum = f1 + f2;
+		f1 = f2;
+		f2 = sum;
+		if (sum < 4000000)
+		{
+			sums = sums + sum;
+		}
+	}
+	printf("%ld\n", sums);
+	return (0);
+}
+
+
+
