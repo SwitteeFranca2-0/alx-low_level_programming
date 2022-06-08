aa8965020357af45706cfee6e5f75ba8d8cb5542
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-08 01:21:28 +0100

    C

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,21 @@
  */
 int main(void)
 {
-	int f1;
-	int f2;
-	int sum;
+	long f1;
+	long f2;
+	long sum;
 	int n;
 
 	f1 = 1;
 	f2 = 2;
-	printf("%d, ", f1);
-	printf("%d", f2);
-	for (n = 2; n <= 50; n ++)
+	printf("%ld, ", f1);
+	printf("%ld", f2);
+	for (n = 2; n < 50; n++)
 	{
 		sum = f1 + f2;
 		f1 = f2;
 		f2 = sum;
-		printf(", %d", sum);
+		printf(", %ld", sum);
 	}
 	printf("\n");
 	return (0);
