cc1d97bbdeee97764c29923bd648f9a22030acb9
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-08 01:00:39 +0100

    C

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print smultiples.
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int sum;
+
+	for (n = 3; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+			sum = sum + n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
