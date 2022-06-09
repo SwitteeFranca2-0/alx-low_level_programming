1f84470c32803c7584ba0689c7d903099c0e84a7
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-09 20:58:14 +0100

    C

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,16 +8,14 @@
 void print_square(int size)
 {
 	int m;
-	int n;
 
-	n = size;
-	if (n == 0)
+	if (size == 0)
 		_putchar('\n');
 
 	while (size > 0)
 	{
-		for (m = 1; m <= n; m++)
-			_putchar('#');
+		for (m = 0; m < n; m++)
+			_putchar(35);
 		size--;
 		_putchar('\n');
 	}
