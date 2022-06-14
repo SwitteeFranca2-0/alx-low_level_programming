07f647f072bf7f2ac5d17e329ebffde45e616e8b
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-14 04:16:57 +0100

    C

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(void)
-{
-	char s1[98];
-	char *ptr;
-
-	ptr = _strcpy(s1, "First, solve the problem.");
-	printf("%s", s1);
-	printf("%s", ptr);
-	return (0);
-}
-
 /**
  * char _strcpy - strcpy.
  * @dest: dest
@@ -20,13 +9,13 @@ int main(void)
  */
 
 char *_strcpy(char *dest, char *src)
-{	
+{
 	int i;
-	
+
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 	dest[i++] = '\0';
-	
+
 	return (dest);
 
 }
