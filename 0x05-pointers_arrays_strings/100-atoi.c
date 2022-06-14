1a053f0e02ded0eb70c60fb13e6e1733a69457c4
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-14 20:31:05 +0100

    C

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"i
+
+/**
+ * _atoi - a to integer.
+ * @s: argument.
+ * Return: inetger.
+ */
+int _atoi(char *s)
+{
+	int i, k;
+	unsigned int m;
+	
+	m = 0;
+	i = 1;
+	for (k = 0; s[k] != '\0'; k++)
+	{	if(s[k] == 45)
+			i = i * -1;
+		if (s[k] >= 48 && s[k] <= 57)
+			m = m*10 + s[k] - '0';
+	}
+	m = m * i;
+
+		
+	return (m);
+}
+
