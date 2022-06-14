15e81da2029709085870578c1db35776534b8185
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-14 21:47:41 +0100

    C

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,12 +12,17 @@ int _atoi(char *s)
 
 	m = 0;
 	i = 1;
+
 	for (k = 0; s[k] != '\0'; k++)
 	{
 		if (s[k] == 45)
 			i = i * -1;
 		if (s[k] >= 48 && s[k] <= 57)
+		{
 			m = m * 10 + s[k] - '0';
+		}
+		if ((!(s[k] >= 48 && s[k] <= 57)) && m > 0)
+			break;
 	}
 	m = m * i;
 	return (m);
