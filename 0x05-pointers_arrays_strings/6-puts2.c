5ea54fd12f3fc448e801cad47035e00ad82df1ce
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-14 01:14:01 +0100

    C

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,15 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, m;
 
 	i = 0;
 	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i += 2;
-	}
+		i++;
+	for (m = 0; m < i; m += 2)
+		if (m % 2 == 0)
+		{
+			_putchar(str[m])
+		}
 	_putchar('\n');
 }
