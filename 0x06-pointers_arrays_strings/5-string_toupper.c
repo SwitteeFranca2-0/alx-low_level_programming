2cda9f948f0be9b09c897a6d4470162244ca2262
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-16 02:50:08 +0100

    C

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,25 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
-int main(void)
-{
-	char str[] = "Look up!\n";
-	char *ptr;
-
-	ptr = string_toupper(str);
-	printf("%s", ptr);
-	printf("%s", str);
-	return (0);
-}
+/**
+ * string_toupper - to upper.
+ * @c: arg
+ * Return: string.
+ */
 
 char *string_toupper(char *c)
 {
-	int a;
+	int a, i;
 
-	if (*c >= 65 && *c < 91)
+	i = 0;
+	while (c[i])
 	{
-		a = *c - 65;
-		*c = 97 + a;
+		if (c[i] >= 97 && c[i] < 123)
+		{
+			a = c[i] - 97;
+			c[i] = a + 65;
+		}
+		i++;
 	}
 	return (c);
 }
