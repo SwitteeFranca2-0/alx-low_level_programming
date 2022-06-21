0d6f810ba2e0be63b28105912f45d1b14f581932
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-21 18:25:43 +0100

    C

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
+
 /**
  * print_diagsums - print
  * @a: a
@@ -10,15 +11,19 @@
 
 void print_diagsums(int *a, int size)
 {
-	int b, sum, sec_sum;
+	int b, n, sum, sec_sum;
 
 	sum = 0;
 	sec_sum = 0;
 
 	for (b = 0; b < size; b++)
 	{
-		sum += *(*(a + b) + b);
-		sec_sum += *(*(a + b) + (size - 1 - b));
+		for (n = 0; n < size; n++)
+		{
+			if (b == n)
+				sum += *(a + (b * size + n));
+		}
+		sec_sum += *(a + (b * size + (size - 1 - b)));
 	}
 
 	printf("%d, %d\n", sum, sec_sum);
