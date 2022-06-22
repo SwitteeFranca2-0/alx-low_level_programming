bf6daf6c5d1c7519ec545733b91635c86646ad5d
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-22 20:07:42 +0100

    C

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,18 +8,15 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
 	if (s[0] != '\0')
 	{
-		putchar(s[0]);
+		_putchar(s[0]);
 		s = s + 1;
 		_puts_recursion(s);
 	}
 	if (s[0] == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 }
