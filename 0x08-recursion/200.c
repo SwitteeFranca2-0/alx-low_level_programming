f1a04892adfb665085d27918b687a2a63794255f
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-23 02:05:44 +0100

    C

--- 0x08-recursion/200.c
+++ 0x08-recursion/200.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int r;
 
-	r = is_palindrome("grep");
+	r = is_palindrome("step on no pets");
 	printf("%d\n", r);
 	return (0);
 }
@@ -26,10 +26,11 @@ int is_palindrome(char *s)
 int is_pal(char *s, int i)
 {
 	if (s[i] == '\0')
+	{
 		i = i - 1;
 		return (is_pals(s, i, 0));
-	i++;
-	return (is_pal(s, i));
+	}
+	return (is_pal(s, i + 1));
 }
 
 int is_pals(char *s, int i, int m)
