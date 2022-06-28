c6394e116f78e9bab13ea606b27f8ceff8f01c3f
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-28 17:36:53 +0100

    C

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = malloc(sizeof(int *) * height);
+	p = malloc(sizeof(int *) * (height - 1));
 
 	if (p == NULL)
 	{
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int *) * width);
+		p[i] = malloc(sizeof(int *) * (width - 1));
 		if (p[i] == NULL)
 		{
 			for (n = i; n >= 0; n--)
