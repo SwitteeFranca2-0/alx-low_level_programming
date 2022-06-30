86176a970b5ed0cff701ff93d10fd39079796cdd
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-06-30 23:12:53 +0100

    C

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - realloc.
+ * @ptr: pointer.
+ * @old_size: initial.
+ * @new_size: final
+ * Return: void.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	int n, i;
+
+
+	p = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < old_size && i <	new_size; i++)
+		p[i] = ((char *) ptr)[i];
+	free(ptr);
+	return (p);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,109 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_error(void);
+void digit_check(char *s);
+long int _strtoi(char *s);
+long long int _mul(long long int a, long long int b);
+
+/**
+ * main - multiply two numbers from the argument.
+ * @argc: number of arguments.
+ * @argv: array of arguments passed in the command line.
+ * Return: return 0.
+ */
+int main(int argc, char *argv[])
+{
+	long long int num1, num2, mul = 1;
+	long long int m, n;
+	char *e;
+
+	if (argc != 3)
+	{
+		print_error();
+		exit(98);
+	}
+
+	digit_check(argv[1]);
+	digit_check(argv[2]);
+
+	num1 = _strtoi(argv[1]);
+	num2 = _strtoi(argv[2]);
+	 
+	mul = _mul(num1, num2);
+
+
+	printf("%lld\n", mul);
+	return (0);
+}
+
+/**
+ * print_error: print "error"
+ * Return: none.
+ */
+void print_error(void)
+{
+	putchar('E');
+	putchar('r');
+	putchar('r');
+	putchar('o');
+	putchar('r');
+	putchar('\n');
+}
+
+/**
+ * error_check: check to makes sure only digts are in passed arguments.
+ * @s: argument.
+ * Return: none.
+ */
+void digit_check(char *s)
+{
+	int m;
+
+	for ( m = 0; s[m] != '\0'; m++)
+	{
+		if (!(s[m] >= 48 && s[m] <= 57))
+		{
+			print_error();
+			exit(98);
+		}
+	}
+}
+
+/**
+ * strtoi - convert string to integre.
+ * @s: string.
+ * Return: integer.
+ */
+long int _strtoi(char *s)
+{
+	int k;
+	long long m;
+
+	m = 0;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		if (s[k] >= 48 && s[k] <= 57)
+		{
+			m = m * 10 + s[k] - '0';
+		}
+	}
+	
+	return (m);
+}
+
+/**
+ * _mul - multiply arguments.
+ * @a: argument.
+ * @b: argument.
+ * Return: product of arguments.
+ */
+long long int _mul(long long int a, long long int b)
+{
+	long long int m;
+
+	m = a * b;
+	return (m);
+}
