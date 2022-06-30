1a41e1bc7e329928d5563569552971ad2ee8ff49
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-07-01 00:09:29 +0100

    C

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,11 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-	{
-		if (p == NULL)
-			return (NULL);
-		return (p);
-	}
+		p = malloc(new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
