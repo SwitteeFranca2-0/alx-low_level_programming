9d2d422b277435cc62432d2d29cd5c88d19b6122
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-07-04 23:44:05 +0100

    C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,26 @@
 #ifndef _HEADER_
 #define _HEADER_
-struct dog {
+
+/**
+ * struct dog - struct dog.
+ * @name: name;
+ * @age: age.
+ * @owner: owner.
+ *
+ * Description: dog.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog_t {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+/**
+ * dog_t - typedef.
+ */
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
