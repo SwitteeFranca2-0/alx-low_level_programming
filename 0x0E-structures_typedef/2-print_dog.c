32a464e35eccd0b1b5d76d9ba39f3b9aacab8060
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-07-04 23:33:45 +0100

    C

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
+		printf("Age: %f\n", (*d).age ? (*d).age : 0);
 		printf("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
-		if ((*d).age == NULL)
-		{
-			printf("(nil)");
-		}
-		else
-		{
-			printf("Age: %f\n", (*d).age);
-		}
 	}
 }
