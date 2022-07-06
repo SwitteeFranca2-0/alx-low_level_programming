4bdc0940d2e9bd0bccd9d177a514e909d5e77ee7
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-07-06 23:29:03 +0100

    C

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main.
+ * @argc: argc.
+ * @argv: argv.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, m;
+	char *o;
+
+	o = (char *)main;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	 m = atoi(argv[1]);
+
+	if (m < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < m; i++)
+	{
+		printf("%.2x", o[i] & 0xFF);
+		if (i < (m - 1))
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
+
