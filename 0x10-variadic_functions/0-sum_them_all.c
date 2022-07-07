64b67c60ce6a480ab2667d46ce87dee66a3906a2
SwitteeFranca2-0 <190401022@live.unilag.edu.ng>
2022-07-08 00:04:29 +0100

    C

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,16 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int m;
 
 	m = 0;
-	va_start (args, n);
+	va_start(args, n);
 
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
-		m += va_arg (args, int);
+		m += va_arg(args, int);
 	va_end(args);
 	return (m);
 }
-	
-
-
