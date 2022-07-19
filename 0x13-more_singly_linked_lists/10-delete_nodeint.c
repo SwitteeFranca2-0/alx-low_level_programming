ef3bb96c46f4d436d6ce9f7544233ad099dc933c
Swittee Franca <190401022@live.unilag.edu.ng>
2022-07-20 00:33:20 +0100

    C

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *p, *m;
-	int h, i;
+	int i;
 
 	m = *head;
 
---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: head.
+ * Return: pointer to the first node in the new list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p, *n;
+
+	p = NULL;
+	n = NULL;
+
+	while (*head)
+	{
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
+
+	}
+
+	*head = p;
+	return (*head);
+}
