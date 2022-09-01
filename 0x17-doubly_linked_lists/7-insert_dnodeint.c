eb2139ad9ced4f4ca0d5691cea5826072a2d0f69
Swittee Franca <190401022@live.unilag.edu.ng>
2022-09-02 00:50:02 +0100

    py

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	}
 
-	if (idx > 0 && i != idx || h == NULL)
+	if (idx > 0 && i != idx || *h == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
