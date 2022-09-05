f41617f54aa81b57f77216edf422a852228f10aa
Swittee Franca <190401022@live.unilag.edu.ng>
2022-09-05 18:56:00 +0100

    py

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,15 +7,33 @@
  */
 int main(void)
 {
-	int m;
-	long long int n, y;
-	long int max;
-	long int num;
+	int m = 0;
+	long long int n, y, x;
+	long int max = 0;
+	long int hum = 0, num;
 	double root;
 
 	n = 612852475143;
-	root = sqrt(n);
+	for (y = 2; y <= n / 4; y++)
+	{
+		num = 2;
+		if (n % y == 0)
+		{
+			{
+				if (y % num == 0)
+					break;
+				num++;
+			}
+		}
+
+		if (num <= (y / 2) - 1)
+			continue;
+		hum = y;
+		if (hum > max)
+			max = hum;
+	}
 	
+	printf("%ld", max);
 	
 	return (0);
 }
