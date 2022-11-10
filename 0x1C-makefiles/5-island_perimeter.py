90c61b6ed9db832e5306e518a9bf35b04fdbaae2
Swittee Franca <190401022@live.unilag.edu.ng>
2022-11-10 03:35:25 +0100

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,8 +6,6 @@
 
 def island_perimeter(grid):
     """This finds the perimeter of the island"""
-    length = 0
-    width = 0
     z_p = 0
     max = 0
     height = len(grid)
@@ -17,8 +15,8 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 max += 1
-            if (j != 0 and grid[i][j - 1] == 1):
+            if (j > 0 and grid[i][j - 1] == 1):
                 z_p += 1
-            if (i != 0 and grid[i-1][j] == 1):
+            if (i > 0 and grid[i-1][j] == 1):
                 z_p += 1
     return max*4 - z_p*2
